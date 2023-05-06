fb4b7b165ce8371b39db428ff04652e9e0a400af
poli8512 <poli8512@gmail.com>
2023-05-06 15:36:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+//import {FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+
+const App = () => {
+	const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+
+	};
+	return (
+		<div>
+		hhh
+		</div>
+	);
+};
+
+export default App;
