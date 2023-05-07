cc9ef177869c01da21282437cf2ddc9fcf728f16
poli8512 <poli8512@gmail.com>
2023-05-07 14:26:50 +0300

    feat:add form

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,37 @@ import React, {useState} from 'react';
 import {Box, FormControl, SelectChangeEvent} from "@mui/material";
 import SelectElement from "./SelectElement";
 import {DatePicker, TimePicker} from "@mui/x-date-pickers";
-export interface ISelectData{
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export interface ISelectData {
 	label: string,
 	range: boolean,
 	value: string,
-	options: Array<string|number>,
+	legend: string,
+	options: Array<string | number>,
 	handler: (e: SelectChangeEvent) => void
 }
+
 const App = () => {
 	const [tower, setTower] = useState<string>(null)
 	const [floor, setFloor] = useState<string>(null)
 	const [room, setRoom] = useState<string>(null)
 
-	const selectData:ISelectData[] = [
+	const selectData: ISelectData[] = [
 		{
 			label: 'Tower',
 			range: false,
 			value: tower,
 			options: ['А', 'Б'],
+			legend: 'Выберете башню',
 			handler: (e: SelectChangeEvent) => setTower(e.target.value as string)
 		},
 		{
 			label: 'Floor',
 			range: true,
 			value: floor,
+			legend: 'Выберете этаж',
 			handler: (e: SelectChangeEvent) => setFloor(e.target.value as string),
 			options: [3, 27],
 		},
@@ -33,24 +40,42 @@ const App = () => {
 			label: 'Room',
 			range: true,
 			value: room,
-			handler: (e: SelectChangeEvent) => setRoom(e.target.value  as string),
+			legend: 'Выберете комнату',
+			handler: (e: SelectChangeEvent) => setRoom(e.target.value as string),
 			options: [1, 10],
 		},
 	]
-const [date,setDate]=useState(null)
+	const [date, setDate] = useState(null)
 	return (
-		<Box>
-			<FormControl fullWidth>
-				{
-					selectData.map(select => {
-							return <SelectElement key={select.label} selectData={select}/>
-					})
-				}
-			</FormControl>
-			<TimePicker label="Basic time picker"/>
-			<DatePicker onChange={(newValue) => setDate(newValue)}/>
+		<form style={{padding:'20px'}}>
+			<fieldset style={{border:'1px solid gray'}}>
+				<legend>Форма бронирования переговорной</legend>
+				<Box>
+					<FormControl fullWidth>
+						{
+							selectData.map(select => {
+								return <fieldset>
+									<legend>{select.legend}</legend>
+									<SelectElement key={select.label} selectData={select}/>
+								</fieldset>
+							})
+						}
+					</FormControl>
+					<fieldset style={{display: 'flex', alignItems: 'center'}}>
+						<legend>Выберете диапозон времени</legend>
+						<TimePicker label="Basic time picker"/>
+						<Typography sx={{margin: '0 20px'}}>-</Typography>
+						<TimePicker label="Basic time picker"/>
+					</fieldset>
+					<fieldset style={{display: 'inline'}}>
+						<legend>Выберете дату</legend>
+						<DatePicker onChange={(newValue) => setDate(newValue)}/>
+					</fieldset>
+				</Box>
+				<Button sx={{marginTop: '20px'}} size="large" variant="outlined"> Готово </Button>
 
-		</Box>
+			</fieldset>
+		</form>
 	);
 };
 
